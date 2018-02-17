dc4080608caf6e25df2ba0d9d6b762fcbceaa0e7
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2018-02-17 15:51:52 +0100

    Retta alle feil i aktiveringsscriptet
    resolves #1 :D :D :D

--- bibliotider.php
+++ bibliotider.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
-Plugin Name:  Åpningstider og romreservering for bibliotek
+Plugin Name:  Bibliotekets åpningstider og reservering av grupperom
 Plugin URI:   https://github.com/olafmoriarty/bibliotider/
 Description:  Registrering og visning av åpningstider, tilpasset bibliotek. Skiller mellom betjent, selvbetjent og meråpent.
 Version:      0.0.1
-Author:       Olaf Moriarty Solstrand, Ski bibliotek
+Author:       Ski bibliotek
 Author URI:   http://skibibliotek.no
 License:      
 License URI:  
@@ -15,18 +15,24 @@ Domain Path:  /sprak
 
 
 // Opprett MySQL-tabeller
-register_activation_hook( __FILE__, array('bibliotider', 'mysql_install'));
+$bibliotider = new bibliotider;
+register_activation_hook( __FILE__, array($bibliotider, 'mysql_install'));
 
 
 
 // ----- Hovedklassen -----
 
 class bibliotider {
-	global $wpdb;
 
-	// Navn på hoved-MySQL-tabellen (andre tabeller har hovedtabellens navn som prefiks)
-	$tabnavn = $wpdb->prefix.'bibliotider';
-	
+	// Wordpress-databasen (settes i tabnavn)
+	var $tabnavn;
+
+	public function __construct() {
+		global $wpdb;
+		$this->wpdb = $wpdb;
+		$this->tabnavn = $wpdb->prefix.'bibliotider';
+	}
+
 	// ----- Opprett MySQL-tabellene -----
 
 	function mysql_install() {
@@ -37,6 +43,7 @@ class bibliotider {
 
 		// Tabell over tider for gitte tidspunkter
 		$tabell = $this->tabnavn;
+
 		$sql = "CREATE TABLE $tabell (
 		  id mediumint(9) NOT NULL AUTO_INCREMENT,
 		  filial tinyint(3) NOT NULL,
@@ -67,11 +74,21 @@ class bibliotider {
 		// Versjonskontroll
 		add_option('bibliotider_tabellversjon', '0.1');
 
-		// Legg til default informasjon om perioder dersom tabellen er tom
+		// Legger til default informasjon om perioder dersom tabellen er tom
 		$num = $wpdb->get_var('SELECT COUNT(*) FROM '.$this->tabnavn.'_perioder');
 		if (!$num) {
-			$wpdb->insert($this->tabnavn.'_perioder', ['navn' => __('Vintertid'), 'startdato' => '2012-09-01', 'sluttdato' => '2012-06-30']);
-			$wpdb->insert($this->tabnavn.'_perioder', ['navn' => __('Sommertid'), 'startdato' => '2012-06-01', 'sluttdato' => '2012-08-31']);
+			$wpdb->insert($this->tabnavn.'_perioder', ['navn' => __('Vintertid', 'bibliotider'), 'startdato' => '2012-09-01', 'sluttdato' => '2012-06-30']);
+			$wpdb->insert($this->tabnavn.'_perioder', ['navn' => __('Sommertid', 'bibliotider'), 'startdato' => '2012-06-01', 'sluttdato' => '2012-08-31']);
+		}
+
+		if (!get_option('bibliotider_filialer')) {
+			// Legger default liste over filialer i options
+			update_option('bibliotider_filialer', array( __('Hovedbiblioteket', 'bibliotider') ));
+		}
+
+		if (!get_option('bibliotider_betjent')) {
+			// Legger default liste over typer åpningstid i options
+			update_option('bibliotider_betjent', array( [__('Betjent', 'bibliotider'), __('Biblioteket er åpent for alle!', 'bibliotider')], [__('Selvbetjent', 'bibliotider'), __('Biblioteket er åpent for alle, men skranken og telefonen er ikke betjent.', 'bibliotider')], [__('Meråpent', 'bibliotider'), __('Biblioteket er stengt, men brukere som har inngått avtale om tilgang til Meråpent bibliotek kan låse seg inn med lånekortet og benytte biblioteket.', 'bibliotider')] ) );
 		}
 
 	}
@@ -83,13 +100,15 @@ class bibliotider {
 		$aar = substr($dato, 0, 4);
 
 		// Finn unntak
-		$query = 'SELECT betjent, starttid, sluttid FROM '.$this->tabnavn.' WHERE filial = '.$filial.' AND '.$dato.' BETWEEN u_startdato AND u_sluttdato ORDER BY betjent';
+		$query = 'SELECT betjent, u_starttid AS starttid, u_sluttid AS sluttid, u_navn AS navn FROM '.$this->tabnavn.' WHERE filial = '.$filial.' AND '.$dato.' BETWEEN u_startdato AND u_sluttdato ORDER BY betjent';
 
-		// *** HENT RADER FRA DATABASEN
+		$result = $wpdb->get_results( $query, OBJECT_K );
 
-		// *** DERSOM num_rows ER NULL
-		$query = 'SELECT t.betjent, t.starttid, t.sluttid FROM '.$this->tabnavn.' AS t LEFT JOIN '.$this->tabnavn.'_perioder AS p ON t.periode = p.id WHERE t.filial = '.$filial.' AND ((p.spesiell = 0 AND '.$d.' BETWEEN DATE_FORMAT(p.startdato, \''.$aar.'-%m-%d\') AND DATE_FORMAT(p.sluttdato, \''.$aar.'-%m-%d\')) OR (p.spesiell = 1 AND ('.$dato.' >= DATE_FORMAT(p.startdato, \''.$aar.'-%m-%d\') OR '.$dato.' <= DATE_FORMAT(p.sluttdato, \''.$aar.'-%m-%d\')))) AND t.ukedag = WEEKDAY('.$dato.') + 1';
+		if (0 == $result->num_rows) {
+			$query = 'SELECT t.betjent, t.starttid, t.sluttid, p.navn FROM '.$this->tabnavn.' AS t LEFT JOIN '.$this->tabnavn.'_perioder AS p ON t.periode = p.id WHERE t.filial = '.$filial.' AND ((p.spesiell = 0 AND '.$d.' BETWEEN DATE_FORMAT(p.startdato, \''.$aar.'-%m-%d\') AND DATE_FORMAT(p.sluttdato, \''.$aar.'-%m-%d\')) OR (p.spesiell = 1 AND ('.$dato.' >= DATE_FORMAT(p.startdato, \''.$aar.'-%m-%d\') OR '.$dato.' <= DATE_FORMAT(p.sluttdato, \''.$aar.'-%m-%d\')))) AND t.ukedag = WEEKDAY('.$dato.') + 1';
+
+			$result = $wpdb->get_results( $query, OBJECT_K );
+		}
+		return $result;
 	}
 }
-
-?
